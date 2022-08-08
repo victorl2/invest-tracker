caa1b17b11a6d9cbfaf2e129c2b767d0b0bb6e22
Victor Silva <victor.silva@grinply.com>
2022-08-08 09:55:56 -0300

    initial project structure

---
+++ src/main/java/com/grinply/investtracker/InvestTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.grinply.investtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestTrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InvestTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/grinply/investtracker/InvestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grinply.investtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
